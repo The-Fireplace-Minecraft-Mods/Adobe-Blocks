a6c0a955af8e5b359b7f86cb53dea32062478f47
The_Fireplace <the.f1repl4ce@gmail.com>
2015-07-05 23:12:57 -0500

    Adobe Wall Recipe

--- src/main/java/the_fireplace/adobeblocks/recipes/VanillaRecipes.java
+++ src/main/java/the_fireplace/adobeblocks/recipes/VanillaRecipes.java
@@ -18,11 +18,13 @@ public class VanillaRecipes {
 	static ItemStack adobeFurnaceStack = new ItemStack(AdobeBlocks.adobe_furnace);
 	static ItemStack adobeMixStack = new ItemStack(AdobeBlocks.adobe_mixture);
 	static ItemStack adobeMixStack32 = new ItemStack(AdobeBlocks.adobe_mixture, 32);
+	static ItemStack adobeWallStack6 = new ItemStack(AdobeBlocks.adobe_wall, 6);
 
 	public static void initRecipes(){
 		GameRegistry.addShapelessRecipe(adobeMixStack32, waterStack, dirtStack, dirtStack, dirtStack, dirtStack, sandStack, sandStack, sandStack, sandStack);
 		GameRegistry.addRecipe(adobeBricksStack, "bb", "bb", 'b', adobeBrickStack);
 		GameRegistry.addRecipe(adobeFurnaceStack, "bbb", "bfb", "bbb", 'b', adobeBricksStack, 'f', furnaceStack);
+		GameRegistry.addRecipe(adobeWallStack6, "bbb", "bbb", 'b', adobeBricksStack);
 
 		GameRegistry.addSmelting(adobeMixStack, adobeBrickStack, 0.3F);
 	}
