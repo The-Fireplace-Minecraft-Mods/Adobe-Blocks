d66a412e58e1504fbd4cfe4d81e3c28f4ab5d7de
The_Fireplace <the.f1repl4ce@gmail.com>
2015-09-16 08:09:53 -0500

    Removed FULCRUM, integrated version checker

--- src/main/java/the_fireplace/adobeblocks/fulcrumcompat/FulcrumCompatDummy.java
+++
@@ -1,9 +0,0 @@
-package the_fireplace.adobeblocks.fulcrumcompat;
-
-public class FulcrumCompatDummy implements IFulcrumCompat {
-
-	@Override
-	public void register() {
-		System.out.println("Unable to check for updates due to missing Fireplace Core");
-	}
-}
--- src/main/java/the_fireplace/adobeblocks/fulcrumcompat/IFulcrumCompat.java
+++
@@ -1,5 +0,0 @@
-package the_fireplace.adobeblocks.fulcrumcompat;
-
-public interface IFulcrumCompat {
-	public void register();
-}
