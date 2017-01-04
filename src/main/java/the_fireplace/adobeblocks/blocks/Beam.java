aa5ee036a39f6154c85aa039a5e8e4e245d3b528
The_Fireplace <the.f1repl4ce@gmail.com>
2017-01-03 20:40:51 -0600

    Updated to 1.11.2

--- src/main/java/the_fireplace/adobeblocks/blocks/Beam.java
+++ src/main/java/the_fireplace/adobeblocks/blocks/Beam.java
@@ -39,7 +39,7 @@ public class Beam extends Block {
 	}
 
 	@Override
-	public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, @Nullable Entity entityIn) {
+	public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, @Nullable Entity entityIn, boolean p_185477_7_) {
 		state = state.getActualState(worldIn, pos);
 		addCollisionBoxToList(pos, entityBox, collidingBoxes, CENTER_AABB);
 
